92c0c6278e969ed7231ddfa48cd8ead9fa8c4599
Zahra15 <the.peace15@gmail.com>
2026-06-08 08:09:09 -0400

    update routes format

--- service/routes.py
+++ service/routes.py
@@ -62,6 +62,8 @@ def create_accounts():
 ######################################################################
 
 # ... place you code here to LIST accounts ...
+
+
 @app.route("/accounts", methods=["GET"])
 def list_accounts():
     """
@@ -72,7 +74,7 @@ def list_accounts():
 
     accounts = Account.all()
     accounts_list = [account.serialize() for account in accounts]
-    
+
     app.logger.info("Returning [%s] accounts", len(accounts_list))
     return jsonify(accounts_list), status.HTTP_200_OK
 
@@ -81,6 +83,8 @@ def list_accounts():
 ######################################################################
 
 # ... place you code here to READ an account ...
+
+
 @app.route("/accounts/<int:account_id>", methods=["GET"])
 def read_accounts(account_id):
     """
@@ -92,7 +96,7 @@ def read_accounts(account_id):
     account = Account.find(account_id)
     if not account:
         abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
-    
+
     return account.serialize(), status.HTTP_200_OK
 
 
@@ -117,13 +121,15 @@ def update_accounts(account_id):
     account.update()
 
     return account.serialize(), status.HTTP_200_OK
-    
+
 ######################################################################
 # DELETE AN ACCOUNT
 ######################################################################
 
 # ... place you code here to DELETE an account ...
-@app.route("/accounts/<int:account_id>",methods=["DELETE"])
+
+
+@app.route("/accounts/<int:account_id>", methods=["DELETE"])
 def delete_accounts(account_id):
     """
     Delete an account
@@ -133,7 +139,7 @@ def delete_accounts(account_id):
     app.logger.info("Request to delete an account with id: %s", account_id)
 
     account = Account.find(account_id)
-    if  account:
+    if account:
         account.delete()
 
     return account.serialize(), status.HTTP_204_NO_CONTENT
